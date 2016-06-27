7399cd6691399ff2634181a6f9fe0bf9486d791c
kevin <liujinsong@dana.com>
2016-06-27 18:01:04 +0800

    init repo

---
+++ BobShop/index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE form PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>orderform</title>
+</head>
+<form action="processorder.php" method="post">
+	<table border="0" align="center">
+		<tr bgcolor="#cccccc">
+			<td width="150px">Item</td>
+			<td width="15px">Quantity</td>
+		</tr>
+		<tr>
+			<td>Tires</td>
+			<td align="center"><input type="text" name="tireqty" size="3"
+				maxlength="3" /></td>
+		</tr>
+		<tr>
+			<td>Oil</td>
+			<td align="center"><input type="text" name="oilqty" size="3"
+				maxlength="3" /></td>
+		</tr>
+		<tr>
+			<td>Spark Plugs</td>
+			<td align="center"><input type="text" name="sparkqty" size="3"
+				maxlength="3" /></td>
+		<tr height="10px"/>
+		<tr/>
+		<tr>
+			<td colspan="2" align="center"><input type="submit"
+				value="Submit Order" /></td>
+		</tr>
+	</table>
+</form>
+</html>
---
+++ BobShop/processorder.php
@@ -0,0 +1,13 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Bob's auto parts--Order Results</title>
+</head>
+<body>
+	<h1>Bob's auto parts</h1>
+	<h2>Order Results</h2>
+	<?php
+echo '<p>Order processed</p>';
+?>
+</body>
+</html>
